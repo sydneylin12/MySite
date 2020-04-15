375ebfb10aa6712673ea4fddb2c6382275fc4a5f
Sid Lin <sydney.lin12@gmail.com>
2020-04-14 21:02:12 -0500

    Updates

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-    $(".menu-toggler").on("click", function(){
-        $(this).toggleClass("open");
-        $(".top-nav").toggleClass("open");
-    })
-
-    $(".nav-list a").on("click", function(){
-        $(".top-nav").toggleClass("open");
-        $(".menu-toggler").toggleClass("open");
-    })
-
     AOS.init({
         easing: "ease",
         duration: 1800
